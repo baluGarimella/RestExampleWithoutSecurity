0b24cfe90156c8149e83e176fef797f041928166
abc <abc@example.com>
2021-11-12 12:48:09 +0530

    firstCommit

---
+++ src/main/java/com/crud/example/crud/example/Product.java
@@ -0,0 +1,49 @@
+package com.crud.example.crud.example;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "product")
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long id;
+	@Column(name = "name")
+	private String name;
+	@Column(name = "price")
+	private float price;
+
+	protected Product() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+}
